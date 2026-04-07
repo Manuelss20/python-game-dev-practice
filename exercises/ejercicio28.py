a3b71c0fa61b8c17c178eef360cad4a3607f930f
Manuel Salgado Enríquez <masaen100@icloud.com>
2026-04-06 23:19:16 -0600

    add a new exercise

---
+++ exercises/ejercicio28.py
@@ -0,0 +1,14 @@
+#supongamos que estamos midiendo el clima
+#solo podemos tener una medicion para una caracteristica
+
+clima_c1= "soleado"
+clima_c2= "nuboso"
+clima_c3= "calido"
+
+#si el clima es soleado y nuboso no necesito paraguas
+#de lo contrario necesito paraguas
+
+if(clima_1=="soleado" and clima_c2=="nuboso"):
+    print("no necesitas paraguas")
+else:
+    print("necesitas un paraguas")
